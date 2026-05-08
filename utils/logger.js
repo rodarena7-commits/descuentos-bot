1c7f6ecad19f9241af3d7c75c52026376f115f5d
Rodrigo Arena <rodrigo@example.com>
2026-05-08 12:11:27 -0300

    Simplificar logger - remover pino-pretty para Render

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,7 @@
 const pino = require('pino');
 
 const logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname'
-        }
-    }
+    level: process.env.LOG_LEVEL || 'info'
 });
 
 module.exports = logger;
